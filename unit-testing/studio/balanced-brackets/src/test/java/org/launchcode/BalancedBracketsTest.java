4f003306ff1ec0ffa314a54317ae27dba19d6125
Owner <braydens.coding@gmail.com>
2024-09-19 20:26:00 -0500

    CH:6 Unit Testing Studio [COMPLETE]

--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -10,4 +10,79 @@ class BalancedBracketsTest {
     public void emptyTest() {
         assertEquals(true, true);
     }
+
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void unbalancedBracketsPassed() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode\""));
+    }
+
+    @Test
+    public void failsIfStringIsRightBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+
+    @Test
+    public void passesWhenPassedNestedBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[]]"));
+    }
+
+    @Test
+    public void textAfterBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]test"));
+    }
+
+    @Test
+    public void doubleRightBracketsReturnFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]]"));
+    }
+
+    @Test
+    public void balancedBracketsWithinUnbalancedBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[][[["));
+    }
+
+    @Test
+    public void flippedBalancedBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void flippedBracketsWithinString() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]Code["));
+    }
+
+    @Test
+    public void passesWhenPassedEmptyString() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void passesWithStringInBalancedBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
+
+    @Test
+    public void textBeforeBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("launchcode[]"));
+    }
+
+    @Test
+    public void textThenTextBetweenBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+    }
+
+    @Test
+    public void multipleBalancedBracketsPassed() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[][][]"));
+    }
+
+    @Test
+    public void multipleUnbalancedBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("][][]["));
+    }
 }
