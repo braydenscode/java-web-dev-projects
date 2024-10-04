cd765c12a97db750366ff147308f6e5fe6e35a23
Owner <braydens.coding@gmail.com>
2024-10-03 20:24:17 -0500

    Added answers to the questions in debugging studio

--- interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/Main.java
+++ interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/Main.java
@@ -9,8 +9,9 @@ public class Main {
     }
 }
 
-//Part 3 Studio answers
+//Studio Answers
+    //Part Three: More Debugger Debugging
 //1. Use a watch expression to keep track of a specific piece of data in your code.
-//2. I don't think so because there isn't a
+//2. I don't think so because a conditional breakpoint only adds a breakout if a condition is met, when here we want to add the breakpoint regardless.
 //3. It shows you where you are at in your code in the process of debugging (method:line, file (package))
-//4. dvd.aName is only used as an argument in the dvd constructor, therefore it doesn't actually exist and the output makes sense.
+//4. dvd.aName is only used as an argument in the dvd constructor, therefore it doesn't actually exist and the output (No such instance field: 'aName') makes sense.
