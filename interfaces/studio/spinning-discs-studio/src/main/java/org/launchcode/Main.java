49fa5d0aa389f19b565913d8e50ee8b8f66c27db
Owner <braydens.coding@gmail.com>
2024-10-03 20:16:31 -0500

    Updating files for checkout

--- interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/Main.java
+++ interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/Main.java
@@ -8,3 +8,9 @@ public class Main {
         // TODO: Call each CD and DVD method to verify that they work as expected.
     }
 }
+
+//Part 3 Studio answers
+//1. Use a watch expression to keep track of a specific piece of data in your code.
+//2. I don't think so because there isn't a
+//3. It shows you where you are at in your code in the process of debugging (method:line, file (package))
+//4. dvd.aName is only used as an argument in the dvd constructor, therefore it doesn't actually exist and the output makes sense.
